a480445cf6065b570190e57228c56917170ba53e
micylt <micylt727@gmail.com>
2017-05-14 22:37:13 -0700

    Made minor style adjustments to map.js.

--- js/map.js
+++ js/map.js
@@ -3,30 +3,30 @@ var map, infoWindow;
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: -34.397, lng: 150.644},
-	  zoom: 6
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 6
 	});
 	infoWindow = new google.maps.InfoWindow;
 
 	// HTML5 geolocation
 	if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-		var pos = {
-		  lat: position.coords.latitude,
-		  lng: position.coords.longitude
-		};
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
 
-		infoWindow.setPosition(pos);
-		infoWindow.setContent('Location found.');
-		infoWindow.open(map);
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('Location found.');
+			infoWindow.open(map);
 
-		map.setCenter(pos);
+			map.setCenter(pos);
 
-	  }, function() {
-		handleLocationError(true, infoWindow, map.getCenter());
-	  });
+		}, function() {
+		       handleLocationError(true, infoWindow, map.getCenter());
+		   });
 	} else {
-	  handleLocationError(false, infoWindow, map.getCenter());
+		handleLocationError(false, infoWindow, map.getCenter());
 	}
 }
 
@@ -34,7 +34,7 @@ function initMap() {
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
 	infoWindow.setContent(browserHasGeolocation ?
-					  'Error: The Geolocation service failed.' :
-					  'Error: Your browser doesn\'t support geolocation.');
-  	infoWindow.open(map);
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+		infoWindow.open(map);
 }
