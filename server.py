975d900119e2616c5db7e5872c354e8373cedf07
micylt <micylt727@gmail.com>
2017-05-21 00:42:25 -0700

    Updated server to handle AJAX position posts. Added test page.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+# NeedleNet Flask server
+from flask import Flask, render_template, request, redirect, Response, jsonify
 import requests
 import requests_cache
 import time
@@ -9,27 +10,27 @@ app = Flask(__name__, template_folder='templates')
 # caches requested data
 requests_cache.install_cache('aid.gov_cache', backend='sqlite')
 
+
 # default route
 @app.route("/")
 def main():
     return render_template('test.html')
 
+
 # redirect (e.g. http://localhost:5000/map)
 @app.route("/map")
 def map():
-    data = get_clinc_locations(47.606, -122.332, 10)
-    print(json.dumps(data, indent=4, sort_keys=True))
     return render_template('view.html')
 
-def get_clinc_locations(lat, lon, dist):
-    """ request clinic data from aids.gov w/ given latitutde, longitude and distance """
-    # round to 3 decimal places
+
+def get_clinic_locations(lat, lon, dist):
+    """ request clinic data from aids.gov w/ given latitude, longitude and distance """
     lat = round(lat, 3)
     lon = round(lon, 3)
     url = "https://locator.aids.gov/data?lat={}&long={}&distance={}".format(lat, lon, dist)
 
     start_time = time.time()
-    print ("getting clinic locations...")
+    print("getting clinic locations...")
     res = requests.get(url, stream=True, allow_redirects=False)
     print('res1 complete')
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -37,6 +38,16 @@ def get_clinc_locations(lat, lon, dist):
     data = res.json()
     return data
 
+
+@app.route('/receiver', methods=['POST'])
+def worker():
+    # read json + reply
+    res = request.get_json()
+    if res is not None:
+        data = get_clinic_locations(res['lat'], res['lon'], res['dist'])
+        return jsonify(data)
+
+    return 'error'
 
 if __name__ == "__main__":
     # detailed log files on errors
